38a1ae2d52ada8d2e35945ff87b8ae9cfe7febff
Sungjin Han <meinside@duck.com>
2025-08-25 16:38:08 +0900

    fix: Fix wrong Janet version

--- amalgamate.sh
+++ amalgamate.sh
@@ -4,12 +4,12 @@
 #
 # This script generates amalgamated janet.c and its header files from Janet repository.
 #
-# last update: 2025.07.31.
+# last update: 2025.08.25.
 
 set -euo pipefail
 
 # NOTE: keep in sync with [janet-lang/janet](https://github.com/janet-lang/janet/releases)
-JANET_VERSION="v1.38.0"
+JANET_VERSION="v1.39.0"
 
 JANET_DIR="vendor/janet"
 AMALGAMATED_DIR="amalgamated"
